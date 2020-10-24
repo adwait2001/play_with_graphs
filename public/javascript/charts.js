a1943f7f170a35635bcb1f61cddd9fccecc9e6f8
ADWAIT <adwaitnsk.2009@rediffmail.com>
2020-10-24 21:24:56 +0530

    fina

--- public/javascript/charts.js
+++ public/javascript/charts.js
@@ -24,21 +24,24 @@ value2 = params.get('time')
 //     value2=curr2.value
 //     console.log(value2)
 
-fetch('/api/linear?curr=' + value + '&curr2=' + value2).then(res => res.json()).then(data => {
-    const cdata = []
-    const element = data["body"]
-    Array.from(element).forEach(element => {
-        cdata.push({ time: element[0] / 1000, value: parseFloat(element[2]) })
-    });
-    lineSeries.setData(cdata);
-    abc();
-})
+
+
+    fetch('/api/linear?curr=' + value + '&curr2=' + value2).then(res => res.json()).then(data => {
+        const cdata = []
+        const element = data["body"]
+        Array.from(element).forEach(element => {
+            cdata.push({ time: element[0] / 1000, value: parseFloat(element[2]) })
+        });
+        lineSeries.setData(cdata);
+    })
+    
 
 
 
-function abc() {    
+setTimeout(() => {
     var ss = chart.takeScreenshot();
     var img = ss.toDataURL("image/png").replace("image/png", "image/octet-stream");
     document.querySelector('span').innerHTML = img;
 
-}
+}, 2000);
+
--- src/utils/api3.js
+++ src/utils/api3.js
@@ -1,33 +1,3 @@
-// const request = require('postman-request')
-// const cheerio=require('cheerio')
-
-// async function stock_api3() {
-
-//     const url='https://finplexgraph.herokuapp.com/graph1?symbol=BTCUSDT&time=5m'
-//     const photos=[]
-//     const response=await request({
-//         uri:url,
-//         headers:{
-//             accept:" */*",
-//             "accept-encoding":" gzip, deflate, br",
-//             "accept-language": "en-US,en;q=0.9,hi;q=0.8",
-//         }
-//     });
-
-
-//     let $=cheerio.load(response)
-//     let img=$('span').text();
-
-//     console.log(img)
-
-//     photos.push({img})
-//     return photos;
-    
-// }
-
-// module.exports=stock_api3;
-
-
 const { json } = require('body-parser');
 const puppeteer=require('puppeteer')
 
@@ -35,20 +5,15 @@ const puppeteer=require('puppeteer')
 async function stock_api3 (symbol,time) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const photos=[]
-  url='https://finplexgraph.herokuapp.com/graph1?symbol=BTCUSDT&time=5m'
-  console.log(url)
-  await page.goto(url);
-
+  await page.goto('http://localhost:5000/graph1?symbol='+symbol+'&time='+time);
+  await page.waitFor(2000)
+  await page.waitForSelector('span')
   const textContent = await page.evaluate(() => {
     return document.querySelector('span').textContent
   });
-
   browser.close();
-
-  
-
   return textContent
+
 };
 
 module.exports=stock_api3;
