a77920d9be3fa0d0f2e9591556b772557e434e3b
ADWAIT <adwaitnsk.2009@rediffmail.com>
2020-12-02 11:40:08 +0530

    cd

--- src/index.js
+++ src/index.js
@@ -37,7 +37,6 @@ const server=app.listen(port,()=>{
 
 
 const io = socket(server);
-io.set('transports', [ 'websocket' ]);
 const bRest = new api.BinanceRest({
   key: "", // Get this from your account on binance.com
   secret: "", // Same for this
